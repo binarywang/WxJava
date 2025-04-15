ba91545eaf0138ea8b7819aa22d0c8c4998ce98e
HerveyHall <herveyhall@foxmail.com>
2025-04-15 11:46:02 +0800

    调整配置参数strictlyNeedWechatPaySerial的默认值，改为默认不添加

--- weixin-java-pay/src/main/java/com/github/binarywang/wxpay/config/WxPayConfig.java
+++ weixin-java-pay/src/main/java/com/github/binarywang/wxpay/config/WxPayConfig.java
@@ -228,9 +228,9 @@ public class WxPayConfig {
   private Verifier verifier;
 
   /**
-   * 是否将全部v3接口的请求都添加Wechatpay-Serial请求头，默认全部添加
+   * 是否将全部v3接口的请求都添加Wechatpay-Serial请求头，默认不添加
    */
-  private boolean strictlyNeedWechatPaySerial = true;
+  private boolean strictlyNeedWechatPaySerial = false;
 
   /**
    * 返回所设置的微信支付接口请求地址域名.
