cc87a2d855d5c88561aec319c0ec283ca9bbdf8c
canxu <1@1.com>
2025-07-04 13:23:59 +0800

    增加字段添加断言判断

--- weixin-java-pay/src/test/java/com/github/binarywang/wxpay/bean/payscore/WxPayScoreRequestTest.java
+++ weixin-java-pay/src/test/java/com/github/binarywang/wxpay/bean/payscore/WxPayScoreRequestTest.java
@@ -2,6 +2,8 @@ package com.github.binarywang.wxpay.bean.payscore;
 
 import org.testng.annotations.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 /**
  * @author <a href="https://github.com/binarywang">Binary Wang</a>
  * created on  2020-07-11
@@ -17,7 +19,11 @@ public class WxPayScoreRequestTest {
       .timeRange(new TimeRange("20230901011023", "20230930235959","开始时间","结束时间"))
       .device(new Device("deviceId","deviceId","212323232"))
       .build();
-    System.out.println(request.toJson());
+    String json = request.toJson();
+    System.out.println(json);
+
+    String expectedJson = "{\"out_order_no\":\"QLS202005201058000201\",\"appid\":\"123\",\"service_id\":\"345\",\"service_introduction\":\"租借服务\",\"time_range\":{\"start_time\":\"20230901011023\",\"end_time\":\"20230930235959\",\"start_time_remark\":\"开始时间\",\"end_time_remark\":\"结束时间\"},\"device\":{\"start_device_id\":\"deviceId\",\"end_device_id\":\"deviceId\",\"materiel_no\":\"212323232\"}}";
+    assertThat(request.toJson()).isEqualTo(expectedJson);
 //    {
 //      "out_order_no": "QLS202005201058000201",
 //      "appid": "123",
