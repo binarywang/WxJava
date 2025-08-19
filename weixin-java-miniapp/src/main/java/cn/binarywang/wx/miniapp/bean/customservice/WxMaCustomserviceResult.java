92bc58b25fc133f2e9ef1c8c6d652b1185cfd7d6
tryking <1437870930#qq.com>
2025-08-19 10:41:34 +0800

    2025-08-19 tryking 增加小程序企微客服相关3个接口 查/增/删

---
+++ weixin-java-miniapp/src/main/java/cn/binarywang/wx/miniapp/bean/customservice/WxMaCustomserviceResult.java
@@ -0,0 +1,50 @@
+package cn.binarywang.wx.miniapp.bean.customservice;
+
+import cn.binarywang.wx.miniapp.bean.express.WxMaExpressAccount;
+import cn.binarywang.wx.miniapp.json.WxMaGsonBuilder;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.gson.annotations.SerializedName;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * 小程序帐号的可选类目，其中 address / tag / title 是提交审核会用到的
+ *
+ * @author <a href="https://github.com/tryking123">tryking123</a>
+ * @since 2025/8/18 17:40
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class WxMaCustomserviceResult  implements Serializable {
+  private static final long serialVersionUID = 8854979405505241314L;
+
+  @SerializedName("errcode")
+  private Integer errCode;
+
+  /**
+   * 该小程序的主体名称，未绑定时不返回
+   */
+  @SerializedName("entityName")
+  private String entityName;
+
+  /**
+   * 企业ID，未绑定时不返回，未绑定时不返回
+   */
+  @SerializedName("corpid")
+  private String corpid;
+
+  /** 接受绑定时间戳，ms */
+  @JsonProperty("bindTime")
+  private Long bindTime;
+
+  public static WxMaCustomserviceResult fromJson(String json) {
+    return WxMaGsonBuilder.create().fromJson(json, WxMaCustomserviceResult.class);
+  }
+}
