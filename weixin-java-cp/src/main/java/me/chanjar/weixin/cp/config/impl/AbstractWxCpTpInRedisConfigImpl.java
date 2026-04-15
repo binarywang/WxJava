5b367746c2e19b763026bacd31d5c18049433b4c
高琪 <gaoqi198@163.com>
2026-04-15 09:19:41 +0800

    Fix: 对expire做toIntExact并用Math.max兜底,避免静默截断

--- weixin-java-cp/src/main/java/me/chanjar/weixin/cp/config/impl/AbstractWxCpTpInRedisConfigImpl.java
+++ weixin-java-cp/src/main/java/me/chanjar/weixin/cp/config/impl/AbstractWxCpTpInRedisConfigImpl.java
@@ -224,7 +224,7 @@ public abstract class AbstractWxCpTpInRedisConfigImpl extends WxCpTpDefaultConfi
 
     WxAccessToken accessTokenEntity = new WxAccessToken();
     accessTokenEntity.setAccessToken(accessToken);
-    accessTokenEntity.setExpiresIn(expire.intValue());
+    accessTokenEntity.setExpiresIn(Math.max(Math.toIntExact(expire), 0));
     return accessTokenEntity;
   }
 
