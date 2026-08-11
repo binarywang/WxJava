067cfd446c481cfab593d584c1f8502fdade6cbe
Binary Wang <a@binarywang.com>
2026-08-11 14:16:01 +0800

    fix: 允许V2沙箱与V3配置共存

--- weixin-java-pay/src/main/java/com/github/binarywang/wxpay/service/impl/WxPayServiceApacheHttpImpl.java
+++ weixin-java-pay/src/main/java/com/github/binarywang/wxpay/service/impl/WxPayServiceApacheHttpImpl.java
@@ -7,6 +7,7 @@ import com.github.binarywang.wxpay.v3.WxPayV3DownloadHttpGet;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import lombok.extern.slf4j.Slf4j;
+import me.chanjar.weixin.common.error.WxRuntimeException;
 import me.chanjar.weixin.common.util.http.apache.ByteArrayResponseHandler;
 import me.chanjar.weixin.common.util.json.GsonParser;
 import org.apache.commons.lang3.StringUtils;
@@ -128,6 +129,7 @@ public class WxPayServiceApacheHttpImpl extends BaseWxPayServiceImpl {
   }
 
   private String requestV3(String url, String requestStr, HttpRequestBase httpRequestBase) throws WxPayException {
+    this.checkV3SandboxNotSupported();
     CloseableHttpClient httpClient = this.createApiV3HttpClient();
     try (CloseableHttpResponse response = httpClient.execute(httpRequestBase)) {
       //v3已经改为通过状态码判断200 204 成功
@@ -163,6 +165,7 @@ public class WxPayServiceApacheHttpImpl extends BaseWxPayServiceImpl {
 
   @Override
   public String postV3WithWechatpaySerial(String url, String requestStr) throws WxPayException {
+    this.checkV3SandboxNotSupported();
     HttpPost httpPost = this.createHttpPost(url, requestStr);
     this.configureRequest(httpPost);
     CloseableHttpClient httpClient = this.createApiV3HttpClient();
@@ -199,6 +202,7 @@ public class WxPayServiceApacheHttpImpl extends BaseWxPayServiceImpl {
 
   @Override
   public String requestV3(String url, HttpRequestBase httpRequest) throws WxPayException {
+    this.checkV3SandboxNotSupported();
     this.configureRequest(httpRequest);
     CloseableHttpClient httpClient = this.createApiV3HttpClient();
     try (CloseableHttpResponse response = httpClient.execute(httpRequest)) {
@@ -243,6 +247,7 @@ public class WxPayServiceApacheHttpImpl extends BaseWxPayServiceImpl {
 
   @Override
   public InputStream downloadV3(String url) throws WxPayException {
+    this.checkV3SandboxNotSupported();
     HttpGet httpGet = new WxPayV3DownloadHttpGet(url);
     this.configureRequest(httpGet);
     CloseableHttpClient httpClient = this.createApiV3HttpClient();
@@ -285,6 +290,7 @@ public class WxPayServiceApacheHttpImpl extends BaseWxPayServiceImpl {
   }
 
   private void configureRequest(HttpRequestBase request) {
+    this.checkV3SandboxNotSupported();
     String serialNumber = getWechatPaySerial(getConfig());
     String method = request.getMethod();
     request.addHeader(ACCEPT, APPLICATION_JSON);
@@ -298,6 +304,12 @@ public class WxPayServiceApacheHttpImpl extends BaseWxPayServiceImpl {
       .setConnectTimeout(this.getConfig().getHttpConnectionTimeout())
       .setSocketTimeout(this.getConfig().getHttpTimeout())
       .build());
+  }
+
+  private void checkV3SandboxNotSupported() {
+    if (this.getConfig().isUseSandboxEnv()) {
+      throw new WxRuntimeException("微信支付V3 目前不支持沙箱模式！");
+    }
   }
 
   private CloseableHttpClient createApiV3HttpClient() throws WxPayException {
