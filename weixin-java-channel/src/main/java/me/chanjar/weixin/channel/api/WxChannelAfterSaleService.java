04e3104f119cdc44a149110f7de3c0360d367218
Binary Wang <a@binarywang.com>
2026-08-22 12:41:26 +0800

    新增视频号保障单服务接口

--- weixin-java-channel/src/main/java/me/chanjar/weixin/channel/api/WxChannelAfterSaleService.java
+++ weixin-java-channel/src/main/java/me/chanjar/weixin/channel/api/WxChannelAfterSaleService.java
@@ -199,4 +199,64 @@ public interface WxChannelAfterSaleService {
    * @throws WxErrorException 异常
    */
   WxChannelBaseResponse merchantUpdateAfterSale(AfterSaleMerchantUpdateParam param) throws WxErrorException;
+
+  /**
+   * 获取保障单列表。
+   * 文档地址：https://developers.weixin.qq.com/doc/channels/API/aftersale/searchguaranteeorder.html
+   *
+   * @param param 查询参数
+   * @return 保障单列表
+   * @throws WxErrorException 异常
+   */
+  GuaranteeOrderListResponse listGuaranteeOrder(GuaranteeOrderListParam param) throws WxErrorException;
+
+  /**
+   * 获取保障单详情。
+   * 文档地址：https://developers.weixin.qq.com/doc/channels/API/aftersale/getguaranteeorder.html
+   *
+   * @param guaranteeOrderId 保障单号
+   * @return 保障单详情
+   * @throws WxErrorException 异常
+   */
+  GuaranteeOrderInfoResponse getGuaranteeOrder(String guaranteeOrderId) throws WxErrorException;
+
+  /**
+   * 同意保障单申请。
+   * 文档地址：https://developers.weixin.qq.com/doc/channels/API/aftersale/merchantacceptguarantee.html
+   *
+   * @param guaranteeOrderId 保障单号
+   * @return 响应结果
+   * @throws WxErrorException 异常
+   */
+  WxChannelBaseResponse acceptGuarantee(String guaranteeOrderId) throws WxErrorException;
+
+  /**
+   * 商家协商保障单。
+   * 文档地址：https://developers.weixin.qq.com/doc/channels/API/aftersale/merchantmodifyguarantee.html
+   *
+   * @param request 协商参数
+   * @return 响应结果
+   * @throws WxErrorException 异常
+   */
+  WxChannelBaseResponse modifyGuarantee(GuaranteeModifyRequest request) throws WxErrorException;
+
+  /**
+   * 商家举证保障单。
+   * 文档地址：https://developers.weixin.qq.com/doc/channels/API/aftersale/merchantproofguarantee.html
+   *
+   * @param request 举证参数
+   * @return 响应结果
+   * @throws WxErrorException 异常
+   */
+  WxChannelBaseResponse proofGuarantee(GuaranteeProofRequest request) throws WxErrorException;
+
+  /**
+   * 拒绝保障单申请。
+   * 文档地址：https://developers.weixin.qq.com/doc/channels/API/aftersale/merchantrefuseguarantee.html
+   *
+   * @param request 拒绝参数
+   * @return 响应结果
+   * @throws WxErrorException 异常
+   */
+  WxChannelBaseResponse refuseGuarantee(GuaranteeRefuseRequest request) throws WxErrorException;
 }
--- weixin-java-channel/src/test/java/me/chanjar/weixin/channel/api/impl/WxChannelAfterSaleServiceImplGuaranteeTest.java
+++ weixin-java-channel/src/test/java/me/chanjar/weixin/channel/api/impl/WxChannelAfterSaleServiceImplGuaranteeTest.java
@@ -1,16 +1,31 @@
 package me.chanjar.weixin.channel.api.impl;
 
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.Arrays;
 import me.chanjar.weixin.channel.bean.after.GuaranteeModifyRequest;
+import me.chanjar.weixin.channel.bean.after.GuaranteeOrderIdParam;
 import me.chanjar.weixin.channel.bean.after.GuaranteeOrderInfoResponse;
+import me.chanjar.weixin.channel.bean.after.GuaranteeOrderListParam;
 import me.chanjar.weixin.channel.bean.after.GuaranteeOrderListResponse;
 import me.chanjar.weixin.channel.bean.after.GuaranteeProofRequest;
 import me.chanjar.weixin.channel.bean.after.GuaranteeRefuseRequest;
+import me.chanjar.weixin.channel.bean.base.WxChannelBaseResponse;
 import org.testng.annotations.Test;
+
+import static me.chanjar.weixin.channel.constant.WxChannelApiUrlConstants.AfterSale.GUARANTEE_ORDER_ACCEPT_URL;
+import static me.chanjar.weixin.channel.constant.WxChannelApiUrlConstants.AfterSale.GUARANTEE_ORDER_GET_URL;
+import static me.chanjar.weixin.channel.constant.WxChannelApiUrlConstants.AfterSale.GUARANTEE_ORDER_LIST_URL;
+import static me.chanjar.weixin.channel.constant.WxChannelApiUrlConstants.AfterSale.GUARANTEE_ORDER_MODIFY_URL;
+import static me.chanjar.weixin.channel.constant.WxChannelApiUrlConstants.AfterSale.GUARANTEE_ORDER_PROOF_URL;
+import static me.chanjar.weixin.channel.constant.WxChannelApiUrlConstants.AfterSale.GUARANTEE_ORDER_REFUSE_URL;
 
 /**
  * 保障单模型 JSON 映射测试。
@@ -61,5 +76,51 @@ public class WxChannelAfterSaleServiceImplGuaranteeTest {
       detailJson, GuaranteeOrderInfoResponse.class);
     assertEquals(detailResponse.getGuaranteeOrder().getGuaranteeOrderId(), "2000001077270153");
     assertEquals(detailResponse.getGuaranteeOrder().getProductInfo().getSkuId(), "456");
+  }
+
+  @Test
+  public void shouldDelegateGuaranteeOrderEndpointsAndDecodeResponses() throws Exception {
+    BaseWxChannelServiceImpl shopService = mock(BaseWxChannelServiceImpl.class);
+    WxChannelAfterSaleServiceImpl service = new WxChannelAfterSaleServiceImpl(shopService);
+    GuaranteeOrderListParam listParam = new GuaranteeOrderListParam();
+    listParam.setOrderId("order-1");
+    GuaranteeModifyRequest modifyRequest = new GuaranteeModifyRequest("guarantee-1", 50, "协商说明");
+    GuaranteeProofRequest proofRequest = new GuaranteeProofRequest("guarantee-1", "举证说明",
+      Arrays.asList("media-1"));
+    GuaranteeRefuseRequest refuseRequest = new GuaranteeRefuseRequest("guarantee-1", "拒绝原因",
+      Arrays.asList("media-2"));
+
+    when(shopService.post(eq(GUARANTEE_ORDER_LIST_URL), eq(listParam))).thenReturn(
+      "{\"errcode\":0,\"total_num\":1,\"guarantee_order_list\":[{\"guarantee_order_id\":\"guarantee-1\"}]}" );
+    when(shopService.post(eq(GUARANTEE_ORDER_GET_URL), eq(new GuaranteeOrderIdParam("guarantee-1")))).thenReturn(
+      "{\"errcode\":0,\"guarantee_order\":{\"guarantee_order_id\":\"guarantee-1\"}}" );
+    when(shopService.post(eq(GUARANTEE_ORDER_ACCEPT_URL), eq(new GuaranteeOrderIdParam("guarantee-1"))))
+      .thenReturn("{\"errcode\":0,\"errmsg\":\"ok\"}");
+    when(shopService.post(eq(GUARANTEE_ORDER_MODIFY_URL), eq(modifyRequest)))
+      .thenReturn("{\"errcode\":0,\"errmsg\":\"ok\"}");
+    when(shopService.post(eq(GUARANTEE_ORDER_PROOF_URL), eq(proofRequest)))
+      .thenReturn("{\"errcode\":0,\"errmsg\":\"ok\"}");
+    when(shopService.post(eq(GUARANTEE_ORDER_REFUSE_URL), eq(refuseRequest)))
+      .thenReturn("{\"errcode\":0,\"errmsg\":\"ok\"}");
+
+    GuaranteeOrderListResponse listResponse = service.listGuaranteeOrder(listParam);
+    GuaranteeOrderInfoResponse detailResponse = service.getGuaranteeOrder("guarantee-1");
+    WxChannelBaseResponse acceptResponse = service.acceptGuarantee("guarantee-1");
+    WxChannelBaseResponse modifyResponse = service.modifyGuarantee(modifyRequest);
+    WxChannelBaseResponse proofResponse = service.proofGuarantee(proofRequest);
+    WxChannelBaseResponse refuseResponse = service.refuseGuarantee(refuseRequest);
+
+    assertEquals(listResponse.getGuaranteeOrderList().get(0).getGuaranteeOrderId(), "guarantee-1");
+    assertEquals(detailResponse.getGuaranteeOrder().getGuaranteeOrderId(), "guarantee-1");
+    assertTrue(acceptResponse.isSuccess());
+    assertTrue(modifyResponse.isSuccess());
+    assertTrue(proofResponse.isSuccess());
+    assertTrue(refuseResponse.isSuccess());
+    verify(shopService).post(GUARANTEE_ORDER_LIST_URL, listParam);
+    verify(shopService).post(GUARANTEE_ORDER_GET_URL, new GuaranteeOrderIdParam("guarantee-1"));
+    verify(shopService).post(GUARANTEE_ORDER_ACCEPT_URL, new GuaranteeOrderIdParam("guarantee-1"));
+    verify(shopService).post(GUARANTEE_ORDER_MODIFY_URL, modifyRequest);
+    verify(shopService).post(GUARANTEE_ORDER_PROOF_URL, proofRequest);
+    verify(shopService).post(GUARANTEE_ORDER_REFUSE_URL, refuseRequest);
   }
 }
